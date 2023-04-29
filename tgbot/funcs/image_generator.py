366bc7db6f3b247043047fa8d6e9e22d39e8430e
matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>
2023-04-29 15:54:48 +0300

    Assigned two variables for regular and bold fonts

--- tgbot/funcs/image_generator.py
+++ tgbot/funcs/image_generator.py
@@ -3,13 +3,13 @@ from PIL import Image, ImageFont, ImageDraw
 
 def get_image(text):
     blank = Image.open("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/blank.png")
-    font = ImageFont.truetype("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/tahoma-bold.ttf", 15)
+    font_regular = ImageFont.truetype("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/tahoma.ttf", 15)
+    font_bold = ImageFont.truetype("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/tahoma-bold.ttf", 15)
     draw = ImageDraw.Draw(blank)
     W = 497
-    w, h = font.getsize(text)
-    draw.text((W - w - 10, 254), text, font=font, fill=(255, 255, 255, 255))
-    font = ImageFont.truetype("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/tahoma.ttf", 15)
-    w, h = font.getsize("Market")
-    draw.text((W - w - 10, 278), "Market", font=font, fill=(255, 255, 255, 255))
+    w, h = font_bold.getsize(text)
+    draw.text((W - w - 10, 254), text, font=font_bold, fill=(255, 255, 255, 255))
+    w, h = font_regular.getsize("Market")
+    draw.text((W - w - 10, 278), "Market", font=font_regular, fill=(255, 255, 255, 255))
     blank.save(f"/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/drawn_image.png", "PNG")
     return open(f"/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/drawn_image.png", "rb")
