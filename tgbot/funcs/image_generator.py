980e961536d9a9a4d0f51208299353dd3e732985
matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>
2023-04-29 16:17:39 +0300

    Raised all constants

--- tgbot/funcs/image_generator.py
+++ tgbot/funcs/image_generator.py
@@ -2,10 +2,10 @@ from PIL import Image, ImageFont, ImageDraw
 
 
 def get_image():
+    W = 497
     blank = Image.open("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/blank.png")
     font_regular = ImageFont.truetype("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/tahoma.ttf", 15)
     font_bold = ImageFont.truetype("/Users/matvejdoroshenko/rendering_bot/tgbot/funcs/tahoma-bold.ttf", 15)
-    W = 497
     draw = ImageDraw.Draw(blank)
     w, h = font_bold.getsize("USD")
     draw.text((W - w - 10, 254), "USD", font=font_bold, fill=(255, 255, 255, 255))
